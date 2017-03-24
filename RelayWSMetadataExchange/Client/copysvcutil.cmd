c1d17a8c97c648e78b4909e68e63877fd05f3b5e
jtaubensee <jotaub@microsoft.com>
2017-03-24 10:35:39 -0700

    Removing samples and updating readme

--- RelayWSMetadataExchange/Client/copysvcutil.cmd
+++
@@ -1,4 +0,0 @@
-for /f "delims=" %%i in ('where svcutil.exe') do set svcutil_loc=%%i
-copy "%svcutil_loc%" .
-for /f "delims=" %%i in ('dir /b/s ..\packages\Microsoft.ServiceBus.dll') do set sbasm_loc=%%i
-copy "%sbasm_loc%" .
